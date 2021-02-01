9a41b18f7a980ef8a41252ef5b63634e41a52772
tianmeng0111 <tianmeng0111@163.com>
2021-02-01 12:00:46 +0800

    add some1

--- app/src/main/java/com/tm/demo/roomapplication/MyGenericObserver.java
+++ app/src/main/java/com/tm/demo/roomapplication/MyGenericObserver.java
@@ -6,6 +6,9 @@ import androidx.lifecycle.GenericLifecycleObserver;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 
+/**
+ * GenericLifecycleObserver 继承LifecycleObserver，多实现了state改变的回调
+ */
 public class MyGenericObserver implements GenericLifecycleObserver {
 
     private static final String TAG = "MyGenericObserver";
